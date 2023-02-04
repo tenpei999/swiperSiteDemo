f57021369ebb92ce88dc89ecdca0277844d693ac
tenpei999 <tenpei999@gmail.com>
2023-02-05 04:33:47 +0900

    [feat]nav_pagenationボタン表示

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,18 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   }
 });
+
+const swiperVideo = new Swiper(".swiper-video", {
+  direction: 'horizontal',
+  // ページネーションが必要なら追加
+  pagination: {
+    el: ".swiper-pagination2",
+    clickable: 'true',
+  },
+  // ナビボタンが必要なら追加
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  }
+});
+console.log(swiperVideo);
