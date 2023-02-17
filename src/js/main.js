e92f49d140c5b0e1765726de56e9bcda352fa3ec
tenpei999 <tenpei999@gmail.com>
2023-02-18 06:36:45 +0900

    [feat]totpo仕組み

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ const swiper = new Swiper(".swiper--main", {
 
   on: {
     init: function () {
-      this.slideTo(this.hashNavigation.getCurrentIndex());
+      this.slideTo(this.hashNavigation);
     }
   },
 
@@ -19,6 +19,8 @@ const swiper = new Swiper(".swiper--main", {
     el: ".swiper-pagination",
     clickable: true, //この行を追記する
   },
+
+  
 });
 
 const swiperVideo = new Swiper(".swiper--video", {
@@ -43,3 +45,19 @@ const swiperVideo = new Swiper(".swiper--video", {
   }
 });
 console.log(swiperVideo);
+
+//swiperでボタンを押したらtopに戻るjavascriptの記述
+
+// イベント関数
+function goTop(){
+  // Swiperオブジェクトを取得
+  const mySwiper = document.querySelector('.swiper--main').swiper;
+  // Swiperのスクロールを最上部に移動
+  mySwiper.slideTo(0);
+}
+
+// HTML要素を取得
+const btn = document.querySelector('.u-button-totop');
+// イベントリスナーを設定
+btn.addEventListener('click', goTop);
+
